f5cdc3e59519c56c7b0831d3a45f627c30a4203c
RJ <19838189+remaruy@users.noreply.github.com>
2018-07-22 12:00:08 +0800

    Add private routes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
-import { setTextFilter } from './actions/filters'
+import { login, logout } from './actions/auth'
 import { startSetExpenses } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';  
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
-// store.dispatch(addExpense({ description: 'Water bill', amount: 300, createdAt: 2111000}));
-// store.dispatch(addExpense({ description: 'Gas bill', amount: 400, createdAt: 1000 }));
-// store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 2111000 }));
-
-// const state = store.getState();
-// console.log(getVisibleExpenses(state.expenses, state.filters));
 
 const jsx = (
   <Provider store={store}>
@@ -26,9 +20,28 @@ const jsx = (
   </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
 });
-
